539e13891715f3544064134a2aeddc5496b7fdd2
KwonJunBeom-4485 <rhfksl0824@gmail.com>
2026-05-18 18:34:09 +0900

    config 수정

---
+++ book-management-api/src/main/java/com/kwonjunbeom/bookmanagementapi/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.kwonjunbeom.bookmanagementapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer { // 💡 여기 인터페이스 명칭을 정확히 확인해 주세요!
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
