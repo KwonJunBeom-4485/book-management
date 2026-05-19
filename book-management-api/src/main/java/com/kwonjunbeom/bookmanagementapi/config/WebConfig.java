7fb813beec0c19c5427b04997b76454499df3919
KwonJunBeom-4485 <rhfksl0824@gmail.com>
2026-05-19 13:36:15 +0900

    배포를 위한 설정4

--- book-management-api/src/main/java/com/kwonjunbeom/bookmanagementapi/config/WebConfig.java
+++ book-management-api/src/main/java/com/kwonjunbeom/bookmanagementapi/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.kwonjunbeom.bookmanagementapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer { // 💡 여기 인터페이스 명칭을 정확히 확인해 주세요!
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
